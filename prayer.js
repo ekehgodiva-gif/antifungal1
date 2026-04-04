7f0a22503f106ec90c9826583b76ff1adf0faa28
ekehgodiva-gif <ekehgodiva@gmail.com>
2026-04-04 15:14:23 +0100

    Update prayer.js

--- prayer.js
+++ prayer.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   prayer: {
-    id: "msg-001", 
-    message: "My Princess, you are brilliant and capable. I pray by his grace, God grants you understanding, clarity and the spirit of excellence in your upcoming exams You've got this! I LOVE YOU 💖"
+    id: "msg-002", 
+    message: "Psalm 46:5<br>God is within you, you will not fall.<br>God will help you at break of day.<br><br>I love you so much. You've got this, my favorite nurse in the world. 💛"
   }
 };
